8d384e1e9688f207e932487403b52df1015ab068
David Neumann <davidjneumann@gmail.com>
2021-12-15 22:57:00 -0500

    Add untested install bash script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#Base settings
+$targetPath = "$HOME/.config/Code/User/settings.json"
+if [[ $OSTYPE == 'darwin'* ]]; then
+  $targetPath = "$HOME/Library/Application Support/Code/User/settings.json"
+fi
+if [ -f $targetPath ]; then
+  $backupPath = $targetPath + ".backup"
+  if [ -f $backupPath]; then
+    rm $backupPath
+  fi
+  cp $targetPath $backupPath
+  rm $targetPath
+fi
+ln -s "$pwd/settings.json" $targetPath
+
+#extensions
+while read p; do
+  code --install-extension $extension
+  echo "$p"
+done <extensions.txt
