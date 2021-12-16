2e2d84197e1f0605c1ca1add1caa0af058be7772
David Neumann <davidjneumann@gmail.com>
2021-12-16 00:31:05 -0500

    Add working macos install .sh

--- install.sh
+++ install.sh
@@ -1,19 +1,30 @@
 #Base settings
-$targetPath = "$HOME/.config/Code/User/settings.json"
+echo "Symlinking your settings.json to this repos settings.json"
+targetPath="$HOME/.config/Code/User/settings.json"
+
 if [[ $OSTYPE == 'darwin'* ]]; then
-  $targetPath = "$HOME/Library/Application Support/Code/User/settings.json"
+  targetPath="$HOME/Library/Application Support/Code/User/settings.json"
 fi
-if [ -f $targetPath ]; then
-  $backupPath = $targetPath + ".backup"
-  if [ -f $backupPath]; then
-    rm $backupPath
+
+if [ -e "$targetPath" ]; then
+  backupPath="$targetPath.backup"
+  if [ -e "$backupPath" ]; then
+    rm "$backupPath"
   fi
-  cp $targetPath $backupPath
-  rm $targetPath
+  cp "$targetPath" "$backupPath"
+  rm "$targetPath"
 fi
-ln -s "$pwd/settings.json" $targetPath
+ln -s "$(pwd)/settings.json" "$targetPath"
 
 #extensions
-while read p; do
-  code --install-extension $extension
+echo "Installing all extensions from extensions.txt"
+while read extension; do
+  echo "Installing $extension"
+  code --install-extension "$extension" > /dev/null
 done <extensions.txt
+
+#macos settings
+if [[ $OSTYPE == 'darwin'* ]]; then
+  echo "Enabling key repeat for macos"
+  defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+fi
